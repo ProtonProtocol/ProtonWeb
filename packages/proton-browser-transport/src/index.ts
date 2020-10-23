39b7859b6f2c501a5b08028d2a46dcf7effbc5cb
Andrew Park <42401371+AndrewPark9517@users.noreply.github.com>
2020-10-23 14:34:26 -0700

    use walletType to adjust wallet modal css (#18)
    * use walletType to adjust wallet modal css
    
    * use object format for scaling
    
    * clean up
    
    * clean up
    
    Co-authored-by: Andrew Park <andrew@troop.work>

--- packages/proton-browser-transport/src/index.ts
+++ packages/proton-browser-transport/src/index.ts
@@ -1,7 +1,7 @@
 import {LinkSession, LinkStorage, LinkTransport} from '@protonprotocol/proton-link'
 import {SigningRequest} from '@protonprotocol/proton-signing-request'
 import * as qrcode from 'qrcode'
-import styles from './styles'
+import styleSelector from './styles'
 
 export interface BrowserTransportOptions {
     /** CSS class prefix, defaults to `@protonprotocol/proton-link` */
@@ -17,6 +17,16 @@ export interface BrowserTransportOptions {
     /** Wallet name e.g. proton, anchor, etc */
     walletType?: string
 }
+
+interface footNoteDownloadLinks {
+    [key: string]: string
+}
+
+const footnoteLinks : footNoteDownloadLinks = {
+    proton: 'https://protonchain.com',
+    anchor: 'https://greymass.com/en/anchor/'
+}
+
 
 class Storage implements LinkStorage {
     constructor(readonly keyPrefix: string) {}
@@ -72,7 +82,7 @@ export default class BrowserTransport implements LinkTransport {
         if (this.injectStyles && !this.styleEl) {
             this.styleEl = document.createElement('style')
             this.styleEl.type = 'text/css'
-            const css = styles[this.walletType].replace(/%prefix%/g, this.classPrefix)
+            const css = styleSelector(this.walletType).replace(/%prefix%/g, this.classPrefix)
             this.styleEl.appendChild(document.createTextNode(css))
             document.head.appendChild(this.styleEl)
         }
@@ -186,7 +196,7 @@ export default class BrowserTransport implements LinkTransport {
         const linkA = this.createEl({
             tag: 'a',
             href: crossDeviceUri,
-            text: 'Open Proton Wallet',
+            text: `Open ${this.walletType.replace(/\b[a-z]/g, (letter) => letter.toUpperCase())} Wallet`,
         })
         linkA.addEventListener('click', (event) => {
             event.preventDefault()
@@ -219,11 +229,11 @@ export default class BrowserTransport implements LinkTransport {
         let footnoteEl: HTMLElement = this.createEl({class: 'footnote'})
         const isIdentity = request.isIdentity()
         if (isIdentity) {
-            footnoteEl = this.createEl({class: 'footnote', text: "Don't have Proton Wallet? "})
+            footnoteEl = this.createEl({class: 'footnote', text: `Don't have ${this.walletType.replace(/\b[a-z]/g, (letter) => letter.toUpperCase())} Wallet? `})
             const footnoteLink = this.createEl({
                 tag: 'a',
                 target: '_blank',
-                href: 'https://protonchain.com',
+                href: footnoteLinks[this.walletType],
                 text: 'Download it here',
             })
             footnoteEl.appendChild(footnoteLink)
